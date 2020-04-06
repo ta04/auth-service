176de931782ab34284666acd80ab1f697a8c9ffe
SleepingNext <veroskevin8@gmail.com>
2020-04-07 05:25:31 +0700

    Connect to python skit-helper

--- handler/handler.go
+++ handler/handler.go
@@ -45,13 +45,19 @@ func (h *handler) AuthRPC2(ctx context.Context, req *authPB.Auth2, res *authPB.T
 	}
 
 	// Calculate the result and compare to T
-	parsedR, _ := strconv.ParseInt(req.R, 10, 64)
+	parsedR, err := strconv.ParseInt(req.R, 10, 64)
+	if err != nil {
+		return err
+	}
 	rInt := int(parsedR)
 	if rInt < 0 {
-		calculateResultUrl := "http://localhost:5000/calculateResult?g=" + user.G + "&r=" + strconv.FormatInt(int64(rInt), 10) + "&n=" + user.N + "&y=" + user.Password + "&c=" + req.C
-		res, _ := http.Get(calculateResultUrl)
-		body, err := ioutil.ReadAll(res.Body)
-		defer res.Body.Close()
+		calculateResultUrl := "http://ec2-54-169-178-240.ap-southeast-1.compute.amazonaws.com/calculateResult?g=" + user.G + "&r=" + strconv.FormatInt(int64(rInt), 10) + "&n=" + user.N + "&y=" + user.Password + "&c=" + req.C
+		calculateResultRes, err := http.Get(calculateResultUrl)
+		if err != nil {
+			return err
+		}
+		body, err := ioutil.ReadAll(calculateResultRes.Body)
+		defer calculateResultRes.Body.Close()
 		if err != nil {
 			return err
 		}
@@ -65,11 +71,31 @@ func (h *handler) AuthRPC2(ctx context.Context, req *authPB.Auth2, res *authPB.T
 
 		result = unmarshalledBody.result
 	} else {
-		g, _ := strconv.ParseFloat(user.G, 64)
-		rFloat, _ := strconv.ParseFloat(req.R, 64)
-		n, _ := strconv.ParseInt(user.N, 10, 64)
-		y, _ := strconv.ParseFloat(user.Password, 64)
-		c, _ := strconv.ParseFloat(req.C, 64)
+		g, err := strconv.ParseFloat(user.G, 64)
+		if err != nil {
+			return err
+		}
+
+		rFloat, err := strconv.ParseFloat(req.R, 64)
+		if err != nil {
+			return err
+		}
+
+		n, err := strconv.ParseInt(user.N, 10, 64)
+		if err != nil {
+			return err
+		}
+
+		y, err := strconv.ParseFloat(user.Password, 64)
+		if err != nil {
+			return err
+		}
+
+		c, err := strconv.ParseFloat(req.C, 64)
+		if err != nil {
+			return err
+		}
+
 		resultTemp := ((int64(math.Pow(g, rFloat)) % n) * (int64(math.Pow(y, c)) % n)) % n
 		result = strconv.FormatInt(resultTemp, 10)
 	}
